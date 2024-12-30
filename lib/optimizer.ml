b4f61cf9810ca98a3ff6e8b38a7a4afa8817f61e
Justin Chai <justinbchai@gmail.com>
2024-12-29 20:29:25 -0500

    tweaked optimizer

--- lib/optimizer.ml
+++ lib/optimizer.ml
@@ -1,7 +1,13 @@
   open Types
   
+
+  let is_int (x: float) : bool = 
+    let y = int_of_float x in
+    x = float_of_int y
+
   let rec optimize e =
     match e with
+    | Const (Float x) -> if is_int x then Const (Int (int_of_float x)) else e
     | Const (_) | Var (_)-> e
     | BinOp (op, e1, e2) ->
       (match op with
@@ -10,11 +16,17 @@
         | Const (Int 0), _ -> optimize e2
         | _, Const (Int 0) -> optimize e1
         | Const (Int x), Const (Int y) -> Const (Int (x + y))
+        | Const (Float x), Const (Float y) -> Const (Float (x +. y))
+        | Const (Float x), Const (Int y) -> Const (Float (x +. float_of_int y))
+        | Const (Int x), Const (Float y) -> Const (Float (float_of_int x +. y))
         | _, _ -> BinOp (Add, optimize e1, optimize e2))
       | Sub -> 
         (match e1, e2 with
         | _, Const (Int 0) -> optimize e1
         | Const (Int x), Const (Int y) -> Const (Int (x - y))
+        | Const (Float x), Const (Float y) -> Const (Float (x -. y))
+        | Const (Float x), Const (Int y) -> Const (Float (x -. float_of_int y))
+        | Const (Int x), Const (Float y) -> Const (Float (float_of_int x -. y))
         | _, _ -> BinOp (Sub, optimize e1, optimize e2))
       | Mul -> 
         (match e1, e2 with
@@ -23,12 +35,19 @@
         | Const (Int 1), _ -> optimize e2
         | _, Const (Int 1) -> optimize e1
         | Const (Int x), Const (Int y) -> Const (Int (x * y))
+        | Const (Int x), Const (Float y) -> Const (Float (float_of_int x *. y))
+        | Const (Float x), Const (Int y) -> Const (Float (x *. float_of_int y))
+        | Const (Float x), Const (Float y) -> Const (Float (x *. y))
         | _, _ -> BinOp (Mul, optimize e1, optimize e2))
       | Div -> 
         (match e1, e2 with
         | _, Const (Int 1) -> optimize e1
         | Const (Int 0), _ -> Const (Int 0)
         | _, Const (Int 0) -> raise DivByZeroError
+        | Const (Int x), Const (Int y) -> Const (Int(x / y))
+        | Const (Float x), Const (Float y) -> Const (Float (x /. y))
+        | Const (Float x), Const (Int y) -> Const (Float (x /. float_of_int y))
+        | Const (Int x), Const (Float y) -> Const (Float (float_of_int x /. y))
         | _, _ -> BinOp (Div, optimize e1, optimize e2))
       | Log -> 
         (match e1, e2 with
